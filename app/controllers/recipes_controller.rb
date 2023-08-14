2c993752e639cb8117dbc4234fc38937d12e58be
Aurelie Chauvin <aurelie.chauvin@essec.edu>
2023-08-14 13:09:41 +0200

    Recipes can be created

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,27 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+    authorize @recipe
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.created_by_id = current_user.id
+    @recipe.save
+    authorize @recipe
+  end
+
+private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :difficulty, :number_of_people, :min_number_of_people)
+  end
 end
---
+++ app/policies/recipe_policy.rb
@@ -0,0 +1,10 @@
+class RecipePolicy < ApplicationPolicy
+  class Scope < Scope
+  end
+  def new?
+    true
+  end
+  def create?
+    true
+  end
+end
